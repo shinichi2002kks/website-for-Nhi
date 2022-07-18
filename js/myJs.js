59db88e5ba03286ed2120b41a3d0765e57e3fdab
shinichi2002kks <109300227+shinichi2002kks@users.noreply.github.com>
2022-07-18 11:58:06 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -138,7 +138,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "https://fb.com/nguyenphan299";
+            window.location = "https://fb.com";
           },
         });
       }
